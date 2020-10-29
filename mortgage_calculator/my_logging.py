a54dad9f4e245686d580c498ac93854b215c4343
Adam Lewis <balast@users.noreply.github.com>
2020-10-29 15:11:53 -0500

    remove file handler logger

--- mortgage_calculator/my_logging.py
+++ mortgage_calculator/my_logging.py
@@ -7,11 +7,6 @@ def get_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
 
-    # create file handler which logs even debug messages
-    log_file_path = Path("mortgage_calculator/logs") / (str(datetime.now()).split(".")[0] + ".log")
-    fh = logging.FileHandler(log_file_path)
-    fh.setLevel(logging.DEBUG)
-
     # create console handler with a higher log level
     ch = logging.StreamHandler()
     ch.setLevel(logging.WARNING)
@@ -21,10 +16,8 @@ def get_logger(name):
         "[%(asctime)s: %(levelname)s/%(filename)s: funcName %(funcName)s - line %(lineno)s]: %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
-    fh.setFormatter(formatter)
     ch.setFormatter(formatter)
 
     # add the handlers to the logger
-    logger.addHandler(fh)
     logger.addHandler(ch)
     return logger
